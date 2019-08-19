6563f5c71805fa85c87df8254393b420878adb54
Zach0702 <50173310+Zach0702@users.noreply.github.com>
2019-08-19 19:09:58 -0400

    first commit

---
+++ AHBC_MIDTERM_2019_JULY_TEAMROCKET/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
