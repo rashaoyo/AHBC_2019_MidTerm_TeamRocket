2af16b2d4d65bf462825bb93c860ae3800f7327e
Zach0702 <50173310+Zach0702@users.noreply.github.com>
2019-08-22 18:38:54 -0400

    commiting story inventory

---
+++ AHBC_MIDTERM_2019_JULY_TEAMROCKET/StoreInventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
+{
+    public class StoreInventory 
+    {
+        public List<StoreItem> TotalStoreInventory { get; set; }
+        public List<StoreItem> GenerateStoreInventory()
+        {
+            List<StoreItem> StoreInventory = new List<StoreItem>();  //creating list to store and return the store inventory
+            StoreItem testItem = new StoreItem(); //Store item creator
+            List<string> storeFromTextFile = new List<string>();  //list to grab the store inventory from text file
+            storeFromTextFile = testItem.FileReader(); //storing text file
+
+            foreach (var line in storeFromTextFile)  //looping through each line of the list to create store items from each line
+            {
+                if (line != "") //if the line is not null continue to add store items to the list
+                {
+                    StoreInventory.Add(testItem.ReturnStoreItem(line));  //adding store items into the store inventory
+                }
+                else //end of the list
+                {
+                    break;
+                }
+            }
+            return StoreInventory; //return the store inventory
+        }
+
+
+    }
+}
---
+++ AHBC_MIDTERM_2019_JULY_TEAMROCKET/StoreItem.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
+{
+    public class StoreItem : IItem
+    {
+        public string NameOfItem { get; set; }
+        public string ItemCategory { get; set; }
+        public int ItemQuantity { get; set; }
+        public double ItemPrice { get; set; }
+
+        //function to read text file
+        public List<string> FileReader() //returns entire file in a list
+        {
+            List<string> linesOfTextFile = new List<string>();  //creating new list to store the text file
+            using (var reader = new StreamReader("Items.txt"))  //opening file
+            {
+                var entireFile = reader.ReadToEnd();  //variable to store the entire file so we can edit
+                var lineArray = entireFile.Split("\r\n");  //storing file into a string array
+
+                foreach (var line in lineArray)  //looping through the line array
+               {
+                    linesOfTextFile.Add(line);  //adding each line of the text file to the lines list
+
+               }
+            }
+            return linesOfTextFile;  //returning the completed list whole text file has been read and stored
+        }
+
+        public StoreItem ReturnStoreItem(string itemLine)  //passing in a line from the list to create an item with
+        {
+            string itemProperties;  //local variable to store the line from the list
+            StoreItem itemCreator = new StoreItem();  //Variable to create our new item 
+            itemProperties = itemLine;  //stores the line passed into the function
+            IList<string> item1 = new List<string>() { }; //list variable to store each property of this item
+            double price = 0; //used for parsing the price into a double
+            int quantity = 0; //used for parsing the quantity into an int
+            //Going through list and splitting on comma because that is how they are in the text file
+            foreach (var property in itemProperties.Split(','))
+            {
+
+                item1.Add(property);  //adding item property to list
+
+            }
+            //storing each property into a store item
+            itemCreator.NameOfItem = item1[0];
+            string temp = item1[1];
+            quantity = int.Parse(temp);
+            //itemCreator.ItemQuantity = item1[1];
+            itemCreator.ItemQuantity = quantity;
+            string temp2 = item1[2];
+            price = double.Parse(temp2);
+            itemCreator.ItemPrice = price;
+            itemCreator.ItemCategory = item1[3];
+
+            //returning created item
+            return itemCreator;
+        }
+
+    }
+}
