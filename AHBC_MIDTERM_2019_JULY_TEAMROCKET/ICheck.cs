818e1dfd894d5abd6fc3e2473defc04515f32c38
victoria <bgreen.victoria@gmail.com>
2019-08-21 21:35:37 -0400

    Added class and interfaces for payment type

---
+++ AHBC_MIDTERM_2019_JULY_TEAMROCKET/ICheck.cs
@@ -0,0 +1,8 @@
+﻿namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
+{
+    internal interface ICheck
+    {
+        void PayWithCheck(double total);
+
+    }
+}
--- AHBC_MIDTERM_2019_JULY_TEAMROCKET/Program.cs
+++ AHBC_MIDTERM_2019_JULY_TEAMROCKET/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
 {
@@ -6,7 +7,53 @@ namespace AHBC_MIDTERM_2019_JULY_TEAMROCKET
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var paymentType = new PaymentType();
+
+            paymentType.PayWithCheck(42.56);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //using (StreamReader sr = File.OpenText("Items.txt"))
+            //{
+            //    // Read the stream to a string, and write the string to the console.
+            //    string[] catLines = System.IO.File.ReadAllLines(@"Items.txt");
+
+            //    foreach (var line in catLines)
+            //    {
+            //         string[] itemLine = line.Split(',');
+
+            //        Console.WriteLine(line);
+
+            //        //if (line.Contains("Clothes") && line.Contains("Dress Shirt"))
+            //        //{
+                        
+            //        //    string[] itemLine = line.Split(',');
+            //        //    Console.WriteLine(itemLine[1]);
+
+            //        //}
+            //    }
+
+
+
+
+            //   // String line = sr.ReadToEnd();
+            //    //Console.WriteLine(line);
+            //}
+
+
+
         }
     }
 }
